fd826813f15d6a24a3f497f35473c4e895438484
babujal <dercamar1@gmail.com>
2023-10-14 19:14:44 -0400

    Data organized in Index.js,data in index body tag ready

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -3,20 +3,30 @@ const Index = (props) => {
 
     const loaded = () => {
         
-        return props.vehicle.map((vehicle) => {
-            return(
-                
-                <div key={vehicle._id} className="vehicle">
-                    <tr>
-                        <th>Make</th>
-                        <th>Description</th>
-                        <th>State</th>
-                        <th>Details</th>
-                    </tr>
-                    <td>{vehicle.vehicle_make}</td>
-                </div>
-            )
-        })
+        return (
+            <div className="vehicle">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Make</th>
+                            <th>Issue</th>
+                            <th>State</th>
+                            <th>Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.vehicle.map((vehicle) => (
+                            <tr key={vehicle._id}>
+                                <td>{vehicle.vehicle_make}</td>
+                                <td>{vehicle.issue_description}</td>
+                                <td>{vehicle.state}</td>
+                                <td>View</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 
     const loading = () => {
